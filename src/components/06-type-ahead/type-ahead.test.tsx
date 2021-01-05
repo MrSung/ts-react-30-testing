33e5cd64d1a872eceaaa510693dce8931e926b44
Sungjoon Park <sungjoon512@gmail.com>
2021-01-05 22:56:31 +0900

    add test for 06-type-ahead

--- src/components/06-type-ahead/type-ahead.test.tsx
+++ src/components/06-type-ahead/type-ahead.test.tsx
@@ -1,3 +1,13 @@
-test.skip("later on", () => {
-  // TODO
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+import { TypeAhead } from "./type-ahead";
+
+describe("06-type-ahead", () => {
+  test("renders a search box with a placeholder City or State and ", () => {
+    render(<TypeAhead />);
+
+    const searchBox = screen.getByPlaceholderText("City or State");
+    expect(searchBox).toBeDefined();
+  });
 });
