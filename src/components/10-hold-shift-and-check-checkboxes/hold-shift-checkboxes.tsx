d63cea276ed5c49cf8ef9a90f41ebd5ab25c9f8b
Sungjoon Park <sungjoon512@gmail.com>
2021-01-10 14:29:22 +0900

    WIP

--- src/components/10-hold-shift-and-check-checkboxes/hold-shift-checkboxes.tsx
+++ src/components/10-hold-shift-and-check-checkboxes/hold-shift-checkboxes.tsx
@@ -4,7 +4,7 @@ import "./style.css";
 
 let i = 0;
 const checkboxTextMap: Map<number, string> = new Map([
-  [i++, "This is an inbox layout"],
+  [i, "This is an inbox layout"],
   [i++, "Check one item"],
   [i++, "Hold down your Shift key"],
   [i++, "This is an inbox layout"],
@@ -17,14 +17,44 @@ const checkboxTextMap: Map<number, string> = new Map([
 ]);
 
 export const HoldShiftCheckboxes: React.FC = () => {
+  const inboxRef = React.useRef<HTMLDivElement>(null);
+  const [lastCheckedId, setLastCheckedId] = React.useState<number | null>(null);
+
+  const handleCheck = (
+    ev: React.ChangeEvent<HTMLInputElement>,
+    currentId: number,
+  ) => {
+    let inBetween = false;
+    console.log("ev.shiftKey ", ev.shiftKey);
+    console.log("ev.target.checked ", ev.target.checked);
+    // @ts-expect-error: KeyboardEvent type annotation not included
+    if (!ev.shiftKey || !ev.target.checked) {
+      return;
+    }
+    [...checkboxTextMap].forEach(([id]) => {
+      if (Number(id) === currentId || Number(id) === lastCheckedId) {
+        inBetween = !inBetween;
+        console.log("Starting to check them in between!");
+      }
+      if (inBetween) {
+        inboxRef.current.querySelector<HTMLInputElement>(
+          `#checkbox-${id}`,
+        ).checked = true;
+        console.log("inBetween true");
+      }
+    });
+    setLastCheckedId(currentId);
+  };
+
   return (
-    <Inbox>
+    <Inbox ref={inboxRef}>
       {[...checkboxTextMap].map(([id, text]) => (
         <Item key={id}>
           <CheckboxInput
             type="checkbox"
             name={`checkbox-${id}`}
             id={`checkbox-${id}`}
+            onChange={(ev) => handleCheck(ev, id)}
           />
           <CheckboxLabel htmlFor={`checkbox-${id}`}>{text}</CheckboxLabel>
         </Item>
